d36d89847339107b49372396eb1052a09ba0d93c
Pacil142857 <7coolaustin7@gmail.com>
2022-05-22 13:16:09 -0400

    Making a Flask app would take too much time, so this is now a console app

--- MetaCriticle.py
+++ MetaCriticle.py
@@ -15,7 +15,6 @@ urls = ['https://www.metacritic.com/game/nintendo-64/tony-hawks-pro-skater-2',
         'https://www.metacritic.com/game/nintendo-64/pokemon-snap',
         'https://www.metacritic.com/game/nintendo-64/super-smash-bros',
         'https://www.metacritic.com/game/wii/super-mario-galaxy',
-        'https://www.metacritic.com/game/wii/super-smash-bros-brawl',
         'https://www.metacritic.com/game/wii/the-legend-of-zelda-skyward-sword',
         'https://www.metacritic.com/game/wii/xenoblade-chronicles',
         'https://www.metacritic.com/game/wii/rayman-origins',
@@ -66,12 +65,12 @@ def getGames():
 
 # Check if a response is correct or not
 # Returns an array of numbers: 0s are gray, 1s are yellow, and 2s are green
-def checkResponse(response, solution):
+def checkResponse(responseTitles, solution):
     feedback = []
     solutionTitles = [game.game['title'] for game in solution]
-    responseTitles = [game.game['title'] for game in response]
+    # responseTitles = [game.game['title'] for game in response]
     
-    for i in range(len(response)):
+    for i in range(len(responseTitles)):
         # Check if this part of the response is correct
         if responseTitles[i] == solutionTitles[i]:
             feedback.append(2)
@@ -85,6 +84,58 @@ def checkResponse(response, solution):
     
     return feedback
 
+def intInput(prompt, min, max):
+    inputGood = False
+    uInput = None
+    while not inputGood:
+        uInput = input(prompt)
+        try:
+            uInput = int(uInput)
+            assert uInput >= min
+            assert uInput <= max
+            inputGood = True
+        except:
+            continue
+    
+    return uInput
+
+gameEnded = False
+confirmSelection = False
+userSelection = []
+solution, distractors = getGames()
+games = [item.game['title'] for item in solution] + [item.game['title'] for item in distractors]
+games.sort()
+hints = [' '] * 26
+
+# Main loop
+print(games)
+while not gameEnded:
+    # Print the available games
+    for i in range(len(games)):
+        print(f'[{hints[i]}] {i + 1}: {games[i]}')
+    
+    # Print the scores of the solution
+    print([item.game['critic_score'] for item in solution])
+    
+    # Get the user's input
+    for i in range(6):
+        userSelection.append(games[intInput(f'Game {i + 1}: ', 1, len(games)) - 1])
+    
+    check = checkResponse(userSelection, solution)
+    
+    # Update games
+    for i, hint in enumerate(check):
+        if hint == 0:
+            del hints[games.index(userSelection[i])]
+            games.remove(userSelection[i])
+        elif hint == 1:
+            hints[games.index(userSelection[i])] = '?'
+        elif hint == 2:
+            hints[games.index(userSelection[i])] = str(i + 1)
+    
+    userSelection = []
+
+'''
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
@@ -104,3 +155,4 @@ def index():
     disImages = [item.game['image'] for item in distractors]
     images = (solImages + disImages).sort()
     return render_template('index.html', images=images)
+'''
