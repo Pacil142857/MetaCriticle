5c8eeaa70652d6ff2ad1c2e02d1a9f50ed026dd8
Pacil142857 <austinmcgregor2@gmail.com>
2022-05-21 22:45:20 -0400

    Started to make it a Flask app

--- MetaCriticle.py
+++ MetaCriticle.py
@@ -1,4 +1,6 @@
 import random
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 from MetaCritic.MetaCriticScraper import MetaCriticScraper
 
 urls = ['https://www.metacritic.com/game/nintendo-64/tony-hawks-pro-skater-2',
@@ -81,3 +83,25 @@ def checkResponse(response, solution):
             feedback.append(0)
     
     return feedback
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config['SESSION_FILE_DIR'] = mkdtemp()
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+
+# Ensure that responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Expires'] = 0
+    response.headers['Pragma'] = 'no-cache'
+    return response
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
