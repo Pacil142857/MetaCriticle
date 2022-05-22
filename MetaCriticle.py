5581c6411b90b3ef9817710a00b5e962e58bc9c4
Pacil142857 <7coolaustin7@gmail.com>
2022-05-22 12:04:50 -0400

    Removed session stuff since it's unnecessary

--- MetaCriticle.py
+++ MetaCriticle.py
@@ -1,6 +1,5 @@
 import random
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
 from functools import wraps
 from MetaCritic.MetaCriticScraper import MetaCriticScraper
 from tempfile import mkdtemp
@@ -89,12 +88,6 @@ def checkResponse(response, solution):
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config['SESSION_FILE_DIR'] = mkdtemp()
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
-
 
 # Ensure that responses aren't cached
 @app.after_request
@@ -106,4 +99,4 @@ def after_request(response):
 
 @app.route('/')
 def index():
-    return render_template('/home/Pacil142857/templates/MetaCriticleindex.html')
+    return render_template('index.html')
